a625d9c533ed9360352bc12a882c5648f51b6212
LivleenS ingh Chani <115384067+livleensinghchani@users.noreply.github.com>
2023-10-22 21:50:03 +0530

    Create GameManager.cs

---
+++ EcoMetropolis/Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+// Global ------------------------
+
+    [SerializeField]
+    private TextMeshProUGUI srcViewMode;
+
+// Local --------------------------
+
+    [SerializeField]
+    private int GameState = 0; //0 = Default, 1 = Road Build, 2 = Block Build
+
+// Function ------------------------
+
+    public void UpdateState(int setState) {
+        switch (setState) {
+            case (0): {
+                GameState = setState;
+                srcViewMode.text = "View";
+                break;
+            }
+            case (1): {
+                GameState = setState;
+                srcViewMode.text = "Road Build";
+                break;
+            }
+            case (2): {
+                GameState = setState;
+                srcViewMode.text = "Block Build";
+                break;
+            }
+            default : {
+                Debug.LogError($"Invalid Game State Request {setState}");
+                break;
+            }
+        }
+    } 
+}
